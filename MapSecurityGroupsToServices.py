04e8d6a6e4832db83198cf09055c0d02bebed2d7
Ashley Gittens <ash.gittens@stablelogic.com>
2024-04-05 16:20:48 +0100

    Got ecs service lookup via SG id working. refactored a bit

--- MapSecurityGroupsToServices.py
+++ MapSecurityGroupsToServices.py
@@ -1,50 +1,6 @@
-from abc import ABC,abstractmethod
 import boto3
 import pandas as ps
-
-class Service(ABC):
-
-    @abstractmethod
-    @staticmethod
-    def get_services(NextToken: str|None):
-        """Return list of services from client"""
-        pass
-    
-    @abstractmethod
-    @staticmethod
-    def return_services_in_security_group(services:list,security_group_id:str):
-        pass
-
-class EC2(Service):
-
-    client=boto3.client('ec2')
-    resource=boto3.resource('ec2')
-
-    @classmethod
-    def get_services(NextToken):
-        pass
-
-    @classmethod
-    def return_services_in_security_group(security_group_id):
-        pass
-
-    @classmethod
-    def get_network_interfaces_for_security_group(security_group_id) -> list:
-        network_interfaces=EC2.client.describe_network_interfaces(
-                Filters=[
-                    {
-                        'Name':'group-id',
-                        'Values':[
-                            f'{security_group_id}'
-                        ]
-                    }
-                ]
-            )['NetworkInterfaces'] 
-
-        return network_interfaces
-
-    def get_service_type_from_network_interface_description(network_interface_description:str) -> Service:
-        pass
+from Services import *
 
 service_types=[
     "Elasticache",
@@ -55,36 +11,8 @@ service_types=[
     "ECS"
 ]
 
-ec2_client=boto3.client('ec2')
+ECS.load_services()
 
-# security_group_response=ec2_client.describe_security_groups()
+test_ecs_services=ECS.get_services_in_security_group("sg-0e0fb363ead3cc7d3")
 
-# security_groups=security_group_response['SecurityGroups']
-
-# security_group_map={
-#     security_group['GroupId']:[security_group['GroupName']].extend(['']*len(service_types)) for security_group in security_groups
-# }
-
-security_groups=[
-    {"GroupId":"sg-039f6c6227fc240b7"}
-]
-
-for security_group in security_groups:
-    ###PLAN
-
-    ##ITERATE THROUGH NETWORK INTERFACES FOR EACH SERVICE GROUP
-    ##TO CHECK WHAT SERVICES NEED RETRIEVING
-    ##LINEAR SEARCH FOR EACH SERVICE TO MATCH SECURITY GROUPS :(
-    
-    response=ec2_client.describe_instances(
-        Filters=[
-            {
-                'Name':'instance.group-id',
-                'Values':[
-                    f'{security_group['GroupId']}'
-                ]
-            }
-        ]
-    )
-
-    print(response)
+print(len(test_ecs_services))
---
+++ Services.py
@@ -0,0 +1,196 @@
+from abc import ABC,abstractmethod
+import boto3
+
+class Service(ABC):
+    """Service ABC, defines a common SG in resources out method
+    common to all services
+    """
+
+    @property
+    @abstractmethod
+    def client():
+        pass
+    
+    @abstractmethod
+    def get_services_in_security_group(services:list,security_group_id:str):
+        pass
+
+class EC2(Service):
+
+    client=boto3.client('ec2')
+    resource=boto3.resource('ec2')
+
+    @classmethod
+    def get_services_in_security_group(cls,security_group_id:str) -> list[dict]:
+        # service_response=cls.client.describe_instances(
+        #     Filters=[
+        #         {
+        #             'Name':'instance.group-id',
+        #             'Values':[
+        #                 f'{security_group_id}'
+        #             ]
+        #         }
+        #     ]
+        # )
+
+        # next_token=service_response['nextToken']
+
+        # for reservation in service_response['reservations']
+
+        pass
+
+
+    @classmethod
+    def get_network_interfaces_for_security_group(cls,security_group_id) -> list[dict]:
+        network_interfaces=cls.client.describe_network_interfaces(
+                Filters=[
+                    {
+                        'Name':'group-id',
+                        'Values':[
+                            f'{security_group_id}'
+                        ]
+                    }
+                ]
+            )['NetworkInterfaces'] 
+
+        return network_interfaces
+
+    @staticmethod
+    def get_service_type_from_network_interface_json(network_interface_json:str) -> Service:
+        description=network_interface_json['Description']
+
+        if "arn:aws:ecs" in description:
+            return ECS
+        
+class NonLookupableService(Service):
+    """For the different services that can't be queried directly for all
+    services corresponding to a given security group.
+
+    These will act the same, but store all their instances/containers etc. beforehand
+    so that I can run my own lookup.
+    
+    This means that I'll only have to do one set of queries for each service.
+    """
+
+    services_by_security_group_id:dict[str,list]={}
+
+    @abstractmethod
+    def load_services():
+        """Loads services to a flat list 
+        """
+        pass
+
+    @classmethod
+    def get_services_in_security_group(cls,security_group_id:str)->list[str]:
+        if not cls.has_services():
+            cls.load_services()
+
+        return cls.services_by_security_group_id[security_group_id]
+
+    @classmethod
+    def has_services(cls):
+        return len(cls.services_by_security_group_id.keys())>0        
+        
+class ECS(NonLookupableService):
+    """Deals with lookup for ECS services
+    """
+
+    client=boto3.client('ecs')
+
+    ###boto3 docs state ecs client.describe_services can only
+    ###take a max of take 10 services at a time
+    lookup_batch_size=10
+
+    @classmethod
+    def load_services(cls) -> None:
+        """Loads services to inherited NonLookupableService.services_by_security_group_id dict
+        by iterating through clusters and getting their ARNs to do the same thing for their
+        individual services.
+
+        From these service ARNs, I can describe them and log what security group they belong to
+        for a later lookup.
+        """
+
+        service_arns_by_cluster_arn={}
+
+        cluster_arns=[]
+
+        cluster_arn_response=cls.client.list_clusters()
+
+        if 'nextToken' in cluster_arn_response.keys():
+            next_token=cluster_arn_response['nextToken']
+        else:
+            next_token=None
+
+        cluster_arns.extend(cluster_arn_response['clusterArns'])
+
+        while next_token!=None:
+            cluster_arn_response=cls.client.list_clusters(
+                nextToken=next_token
+            )
+
+            if 'nextToken' in cluster_arn_response.keys():
+                next_token=cluster_arn_response['nextToken']
+            else:
+                next_token=None
+
+            cluster_arns=cluster_arn_response['clusterArns']
+
+        for cluster_arn in cluster_arns:
+            service_arns=[]
+
+            service_arn_response=cls.client.list_services(
+                cluster=cluster_arn
+            )
+
+            if 'nextToken' in service_arn_response.keys():
+                next_token=service_arn_response['nextToken']
+            else:
+                next_token=None
+
+            service_arns.extend(service_arn_response['serviceArns'])
+
+            while next_token!=None:
+                service_arn_response=cls.client.list_services(
+                    cluster=cluster_arn,
+                    nextToken=next_token
+                )
+
+                if 'nextToken' in service_arn_response.keys():
+                    next_token=service_arn_response['nextToken']
+                else:
+                    next_token=None
+                
+                service_arns.extend(service_arn_response['serviceArns'])
+
+            service_arns_by_cluster_arn[cluster_arn]=service_arns
+
+        for cluster_arn in service_arns_by_cluster_arn.keys():
+
+            service_arns=service_arns_by_cluster_arn[cluster_arn]
+
+            service_arns_len=len(service_arns)
+
+            for service_arn_index in range(0,service_arns_len,cls.lookup_batch_size):
+                
+                if service_arns_len-service_arn_index<cls.lookup_batch_size:
+                    service_response=cls.client.describe_services(
+                        cluster=cluster_arn,
+                        services=service_arns[service_arn_index:]
+                    )
+                else:
+                    service_response=cls.client.describe_services(
+                        cluster=cluster_arn,
+                        services=service_arns[service_arn_index:cls.lookup_batch_size]
+                    )
+                
+                for service in service_response['services']:
+                    security_groups=service['networkConfiguration']['awsvpcConfiguration']['securityGroups']
+
+                    for security_group in security_groups:
+                        if security_group not in cls.services_by_security_group_id.keys():
+                            cls.services_by_security_group_id[security_group]=[service]
+                        else:
+                            cls.services_by_security_group_id[security_group].append(service)
+
+        return
